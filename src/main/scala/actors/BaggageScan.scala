f1fb8b4a23b80a558b934bec43622c05dfa92ade
Bill Dybas <wmdybas@gmail.com>
2016-12-02 18:15:33 -0500

    File Structure

--- src/main/scala/actors/BaggageScan.scala
+++ src/main/scala/actors/BaggageScan.scala
@@ -1,10 +1,12 @@
+package actors
+
 import akka.actor.Actor
 import akka.actor.Props
 import akka.event.Logging
- 
-class Jail extends Actor {
+
+class BaggageScan extends Actor {
   val log = Logging(context.system, this)
- 
+
   def receive = {
     case "test" => log.info("received test")
     case _      => log.info("received unknown message")
--- src/main/scala/actors/Jail.scala
+++ src/main/scala/actors/Jail.scala
@@ -1,10 +1,12 @@
+package actors
+
 import akka.actor.Actor
 import akka.actor.Props
 import akka.event.Logging
- 
-class Queue extends Actor {
+
+class Jail extends Actor {
   val log = Logging(context.system, this)
- 
+
   def receive = {
     case "test" => log.info("received test")
     case _      => log.info("received unknown message")
