95fcd18ec749fd102eb4fbff25beba3dce17078b
Bill Dybas <wmdybas@gmail.com>
2016-12-02 19:15:38 -0500

    Fix Line Endings

--- src/main/scala/actors/SecurityStation.scala
+++ src/main/scala/actors/SecurityStation.scala
@@ -5,9 +5,14 @@ import akka.actor.Props
 import akka.event.Logging
 
 class SecurityStation extends Actor {
-  val log = Logging(context.system, this)
+  // What line number
+  // body and baggage scan results for each passenger
 
   def receive = {
+
+
+
+
     case "test" => log.info("received test")
     case _      => log.info("received unknown message")
   }
